05f48efe29ff47d2a02ebe197cd2b64c978811db
LEANDRO <leandroleo4916@gmail.com>
2020-12-30 22:30:36 -0300

    Add Button

--- app/src/main/java/com/example/app_point/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/app_point/ui/home/HomeFragment.kt
@@ -14,14 +14,12 @@ class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
+
         val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
