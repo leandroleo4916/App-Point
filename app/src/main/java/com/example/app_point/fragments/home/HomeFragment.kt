f11a2ba5cce68cf18d49b2dce067978994ba0901
LEANDRO <leandroleo4916@gmail.com>
2021-01-07 08:57:30 -0300

    Add Button

--- app/src/main/java/com/example/app_point/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/app_point/fragments/home/HomeFragment.kt
@@ -7,8 +7,10 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.example.app_point.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HomeFragment : Fragment() {
 
@@ -16,12 +18,17 @@ class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
 
-
+        val float = root.findViewById(R.id.float_button_point) as FloatingActionButton
+        float.setOnClickListener { registerPoints() }
 
         return root
     }
+
+    private fun registerPoints(){
+
+    }
+
 }
