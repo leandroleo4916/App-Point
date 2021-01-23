682d71229cb18ee0211d9a05556502e4a6becb04
LEANDRO <leandroleo4916@gmail.com>
2021-01-22 23:25:57 -0300

    Modification Xml

--- app/src/main/java/com/example/app_point/activitys/RegisterUser.kt
+++ app/src/main/java/com/example/app_point/activitys/RegisterUser.kt
@@ -6,9 +6,12 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.example.app_point.R
+import com.example.app_point.repository.ReposiitoryUser
 import kotlinx.android.synthetic.main.register_user.*
 
 class RegisterUser : AppCompatActivity(), View.OnClickListener {
+
+    private val mReposiitoryUser: ReposiitoryUser = ReposiitoryUser(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +20,7 @@ class RegisterUser : AppCompatActivity(), View.OnClickListener {
         listener()
     }
 
-    fun listener(){
+    private fun listener(){
         image_back.setOnClickListener(this)
         text_inicio.setOnClickListener(this)
         buttom_register_user.setOnClickListener(this)
@@ -32,7 +35,21 @@ class RegisterUser : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun registerUser(){
+        val name = edittext_username.text.toString()
+        val email = edittext_email.text.toString()
+        val senha = edittext_senha.text.toString()
+        val confirmeSenha = edittext_confirme_senha.text.toString()
 
+        when {
+            name == "" || email == "" || senha == "" || confirmeSenha == "" -> {
+                Toast.makeText(this, "Preencha todos os campos!", Toast.LENGTH_SHORT).show()
+            }
+            senha != confirmeSenha -> {
+                Toast.makeText(this, "As senhas estão diferentes!", Toast.LENGTH_SHORT).show()
+            }
+            mReposiitoryUser.getUser(name, email, senha)->{
+                startActivity(Intent(this, MainActivity::class.java))
+            }
+        }
     }
-
 }
