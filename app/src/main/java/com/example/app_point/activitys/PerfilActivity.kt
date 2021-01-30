34429acce5685a9f74524393b8600b18a1692cda
LEANDRO <leandroleo4916@gmail.com>
2021-01-30 13:27:20 -0300

    Modification Xml

---
+++ app/src/main/java/com/example/app_point/activitys/PerfilActivity.kt
@@ -0,0 +1,12 @@
+package com.example.app_point.activitys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.app_point.R
+
+class PerfilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil)
+    }
+}
