4a10be5f4752543a72024728a76c195e398a5652
LEANDRO <leandroleo4916@gmail.com>
2021-06-03 16:30:52 -0300

    Implementation News

--- app/src/main/java/com/example/app_point/activitys/ToolsActivity.kt
+++ app/src/main/java/com/example/app_point/activitys/ToolsActivity.kt
@@ -3,13 +3,25 @@ package com.example.app_point.activitys
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.app_point.R
+import com.example.app_point.model.AdapterPoints
+import com.example.app_point.model.PointsAdapter
 import kotlinx.android.synthetic.main.activity_tools.*
 
 class ToolsActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var  mEmployeeAdapter: PointsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tools)
+
+        val recycler = findViewById<RecyclerView>(R.id.recycler_employee)
+        recycler.layoutManager = LinearLayoutManager(this)
+        mEmployeeAdapter = PointsAdapter(application)
+        recycler.adapter = mEmployeeAdapter
 
         listener()
     }
